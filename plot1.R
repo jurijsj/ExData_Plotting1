384feb98b7ae4af1c41353aaf257c811969741d2
Jurijs Jefimovs <jurijs.jefimovs@gmail.com>
2014-06-05 17:36:30 +0300

    download file added

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "power_consumption.zip")
+unzip("power_consumption.zip")
 data<- read.delim("household_power_consumption.txt", sep=";", dec=".", na.strings="?", skip = 66636, nrows=2880)
 names(data) <-tolower(c("Date", "Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 png(filename="plot1.png", width=480, height=480)
